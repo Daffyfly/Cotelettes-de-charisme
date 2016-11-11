7a8015198a5c45eb2980068c69eac8392bc765e7
Rémy Kaloustian <remy.kaloustian@gmail.com>
2016-11-11 10:24:03 +0100

    Adding the functions in Robot.h :muscle: :sunglasses:

---
+++ Robot.h
@@ -0,0 +1,33 @@
+#pragma once
+#pragma once
+
+
+#include<string>
+using std::string;
+
+class Robot
+{
+private :
+    string _direction;
+
+    Position& _position;
+    Plot* _plot;
+    Objet* _objet;
+
+    public:
+        Robot();
+        ~Robot();
+
+        void avancer(int x, int y);
+        void tourner(string direction);
+        void saisir(Objet o);
+        void poser();
+        int peser();
+        void rencontretPlot(Plot p);
+        int evaluerPlot();
+        void figer();
+        void repartir();
+        void afficher();
+
+};
+
